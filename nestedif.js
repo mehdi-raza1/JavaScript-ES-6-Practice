e6bbe0f617acefdeb2620f6a8130745e64171f8e
mehdi-raza1 <mraza.valyani@gmail.com>
2024-08-16 19:05:56 +0500

    lecture completed nested if

---
+++ nestedif.js
@@ -0,0 +1,17 @@
+// Nested if condition  if the statement is true it moves in next condition if its false it wll leave it
+
+
+
+const bankAccountBaance = 124
+const costOfItem = 123
+const tax = 0.50
+
+
+
+if(bankAccountBaance > costOfItem){
+    console.log("balance ok, checking tax")
+    if(tax>=0.50){
+        console.log("tax Amount is too high")
+    
+    }
+}
