2875fd67e50d714242ba85f79177e3505600e2d2
mehdi-raza1 <mraza.valyani@gmail.com>
2024-08-16 18:26:46 +0500

    complete lecture of introduction if/else statement

---
+++ ifstatements.js
@@ -0,0 +1,11 @@
+// IF SOME THIMG IS TRUE,RUN MORE CODE 
+
+const shouldSayHi = true
+
+// both are same u can check condition by both 
+if(shouldSayHi){
+    console.log("hi")
+}
+if(shouldSayHi === true ){
+    console.log("hi")
+}
