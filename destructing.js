94d09f615e57396a116fa085dd76373df9a08e60
mehdi-raza1 <mraza.valyani@gmail.com>
2024-08-19 20:09:58 +0500

    lecture completed destructing.js

---
+++ destructing.js
@@ -0,0 +1,28 @@
+const myObj = {
+    firstname: "chris",
+    lastname:"jones",
+    age:16,
+    heigth:'6ft'
+};
+
+const {firstname,lastname,age,heigth} = myObj
+
+console.log(firstname,lastname,age,heigth)
+
+
+// let myvar1 , myvar2;
+// const myObj2 = {
+//     myvar1:'a',
+//     myVar2:'b'
+// }
+// ({myvar1,myvar2} = myvar2)
+// console.log(myvar1,myvar2)
+
+
+
+//  ASSINGING A NEW VARIABLE NAME 
+
+const myObj3 = {a: 'cool!', b: 'oh yeah', c: 'nice', d: 'awesome'};
+const {a, b, c, d} = myObj3;
+console.log(c);
+ 
