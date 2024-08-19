2168f14000347d324c1e1bd3737e001e9b7cb37d
mehdi-raza1 <mraza.valyani@gmail.com>
2024-08-19 20:29:12 +0500

    lecture completed throwing exception

---
+++ throwingexception.js
@@ -0,0 +1,46 @@
+// THROW YOUR OWN EXCEPTION
+
+/*
+    -  USE THE THROW  STATMENTTO THROW OWN EXCEPTION
+    -  YOU SPECIFY THE EXPRESSION CONTAINING THE VALUE TO BE THROWN
+    EG: THROWN EXPRESSIN
+    -  CAN THROW ANY EXPRESSION
+
+*/
+
+
+
+
+// const myObj = {
+//     a:1,
+//     b:2
+// }
+
+// myObj.map((obj)=>console.log(obj))
+
+function checkIfNum(n){
+    if(isNaN(n)){
+        throw 'this is not a number'
+    }else{
+        console.log(n)
+    }
+}
+// checkIfNum("hi")
+checkIfNum(1)
+
+
+const myObjException = {
+    toString: function(){
+        return "i am object exception"
+    }
+}
+
+function MyException(message){
+    this.message = message
+    this.name = "my exception"
+    this.toString = function(){
+        return this.name + " " + this.message
+    }
+}
+
+throw new MyException("field Invalid")
